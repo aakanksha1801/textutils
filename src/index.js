8ec2b87419ae6127376f3d4654819f6efbcc3d3a
Aakanksha Jaiswar <aakankshajaiswar_it20b9_58@dtu.ac.in>
2022-06-23 12:02:08 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import About from './components/About';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-   
-   <App />
-    
-  </Router>
-  
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
